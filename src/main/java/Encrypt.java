daa48047e8a03edac4b2cc2543924d9ec92a730a
Joseph Gakunga <josehwilddog@gmail.com>
2020-01-28 17:15:37 +0300

    create a class constructor

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,4 +1,9 @@
 public class Encrypt {
     private String inputText;
     private int key;
+
+    public Encrypt(String inputText, int key) {
+        this.key = key;
+        this.inputText = inputText;
+    }
 }
