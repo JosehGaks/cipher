7b532a1271bc868e55cc7884ad1af53af71438a6
Joseph Gakunga <josehwilddog@gmail.com>
2020-01-28 17:32:21 +0300

    create a switch method

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,6 +6,7 @@ public class main {
         Encrypt encript = new Encrypt("", 0);
         System.out.println("choose the action you want:\n [1] ENCRYPT\n [2] DECRYPT");
         int x = Integer.parseInt(keyboard.next());
+
         switch (x){
             case 1:
                 System.out.println("\nEnter the desired text u want to ENCRYPT:\n");
@@ -14,6 +15,7 @@ public class main {
                 encript.setKey(Integer.parseInt(keyboard.next()));
                 System.out.println("\nYour encrypted text is::    "+encript.encrypt());
                 break;
+
             case 2:
                 System.out.println("\nEnter the desired text u want to DECRYPT:\n");
                 encript.setInput(keyboard.next());
@@ -21,8 +23,10 @@ public class main {
                 encript.setKey(Integer.parseInt(keyboard.next()));
                 System.out.println("\nYour decrypted text is::   "+encript.decrypt());
                 break;
+
             default:
                 System.out.println("pliz choose!!");
+
         }
     }
 }
