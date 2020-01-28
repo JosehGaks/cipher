4f825f8b185c45bc99ae9f1356983b2376c6b1d3
Joseph Gakunga <josehwilddog@gmail.com>
2020-01-28 18:05:25 +0300

    Create a test decrypt

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,2 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class EncryptTest {
+    @Test
+    public  void  encrypt_encryptText(){
+        Encrypt testEncrypt = new Encrypt("w",2);
+        String expectedOutput = "y";
+        assertEquals(expectedOutput,testEncrypt.encrypt());
+    }
+    @Test
+    public  void  decrypt_decryptText(){
+        Encrypt testDecrypt = new Encrypt("y",2);
+        String expectedOutput = "w";
+        assertEquals(expectedOutput,testDecrypt.encrypt());
+    }
 }
