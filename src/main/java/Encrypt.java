28d8bf6b99bb312021d250e926109206445523fe
Joseph Gakunga <josehwilddog@gmail.com>
2020-01-28 17:24:15 +0300

    create an decrypt method

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -48,4 +48,30 @@ public class Encrypt {
         }
         return output;
     }
+    public String decrypt() {
+        String outputd = "";
+        char c;
+        for (int i = 0; i < inputText.length(); i++) {
+            c = inputText.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                c = (char) (c - key);
+                if (c > 'z') {
+                    c = (char) (c + 'z' - 'a' + 1);
+
+                }
+                outputd += c;
+
+            } else if (c >= 'A' && c <= 'Z') {
+                c = (char) (c - key);
+                if (c >= 'Z') {
+                    c = (char) (c + 'Z' - 'A' + 1);
+                }
+                outputd += c;
+            } else {
+                outputd += c;
+            }
+
+        }
+        return outputd;
+    }
 }
