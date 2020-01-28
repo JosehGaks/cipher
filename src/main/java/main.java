bcac024a643ef781b5480f676dc84de1585da605
Joseph Gakunga <josehwilddog@gmail.com>
2020-01-28 17:33:55 +0300

    create an encript object

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
-        Scanner keyboard = new Scanner(System.in);
         Encrypt encript = new Encrypt("", 0);
+        Scanner keyboard = new Scanner(System.in);
+
         System.out.println("choose the action you want:\n [1] ENCRYPT\n [2] DECRYPT");
         int x = Integer.parseInt(keyboard.next());
 
